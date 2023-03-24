41f5f8766d9830ccb7b94c3ad872b8ef7e5dd93e
Pasha <p.balahonov@corp.mail.ru>
2023-03-24 21:34:13 +0300

    Fix

--- jobs/scripts/jobs_template.groovy
+++ jobs/scripts/jobs_template.groovy
@@ -1,7 +1,8 @@
 timeout(60) {
    node('maven-slave') {
       stage("Checkout") {
-         checkout scm
+         //checkout scm
+        git branches: master, credentialsId: jenkins, url: git@github.com:saint88/jenkins.git
       }
       stage("Deploy changes to jenkins") {
          sh "jenkins-jobs --conf ${WORKSPACE}/jobs/conf/jenkins-job-builder.ini update ./jobs"
